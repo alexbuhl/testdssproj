8618048add909ccd66825ed1db1907706a0b5c63
admin <admin>
2021-02-02 10:14:27 -0500

    Saved web app jswebapp (CUSTOMER_LIFETIME_VALUE.PioXXS4)

--- web_apps/PioXXS4/app.js
+++ web_apps/PioXXS4/app.js
@@ -30,7 +30,7 @@ datasetSelector.onchange = function(event) {
 }
 
 function download_dataset(dataset_name) {
-    let path = '/projects/'+dataiku.defaultProjectKey+'/datasets/'+dataset_name+'/data?filter=mae>12?format=json';
+    let path = '/projects/'+dataiku.defaultProjectKey+'/datasets/'+dataset_name+'/data?filter=date>2021-02-01';
     dataikuREST(path, function(res) {
         console.log(res);
     })
